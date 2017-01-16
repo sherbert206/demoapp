c1d3b8d2658e9bcaccf05cfd2f8e63db48488cfe
Gabriel Flores <sherbert206@gmail.com>
2017-01-16 02:35:06 -0600

    scaffolded user creation and micropost creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116065139) do
+ActiveRecord::Schema.define(version: 20170116072358) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
